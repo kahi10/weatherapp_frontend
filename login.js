b5bb054ec1e73915b2e5b6fff424d279bcfb9a3c
Kahina <bndkahi73@gmail.com>
2025-01-26 14:56:19 +0100

    first commit

---
+++ login.js
@@ -0,0 +1,38 @@
+// Insert your code here
+
+document.querySelector('#register').addEventListener('click', function() {
+    const signup = {
+        name: document.querySelector('#registerName').value,
+        email: document.querySelector('#registerEmail').value,
+        password: document.querySelector('#registerPassword').value
+    };
+    fetch('http://localhost:3000/users/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(signup)
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.result) {
+            window.location.assign('index.html');
+        }
+    })
+});
+document.querySelector('#connection').addEventListener('click', function() {
+    const signin = {
+        email: document.querySelector('#connectionEmail').value,
+        password: document.querySelector('#connectionPassword').value,
+    }
+    fetch('http://localhost:3000/users/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(signin)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+            if (data.result) {
+                window.location.assign('index.html')
+        }
+    })
+})
